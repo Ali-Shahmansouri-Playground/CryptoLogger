e1e8130e78394fba95104abf54e5c3da8ea9c3ca
Ali-Shahmansouri-Playground <sctshahali@gmail.com>
2023-10-12 20:25:36 +0330

    Implement request timer interval

--- Program.cs
+++ Program.cs
@@ -3,26 +3,43 @@ using CryptoLogger.Services;
 using CryptoLogger.Utils;
 
 const string CURRENCY_KEY = "BTC";
+const int REFRESH_INTERVAL_SECONDS = 5;
 
-var apiWrapper = await RequestHandler<ApiResponse>.GetDataAsync();
+int prevTick = 0;
 
-Dictionary<string, Currency> CurrenciesById = new();
-
-foreach (var i in apiWrapper.Data)
+while (true)
 {
-    CurrenciesById.Add(i.Id, i);
+    int curTick = DateTime.Now.Second;
+
+    if (curTick != prevTick)
+    {
+        await InitAsync();
+        prevTick = curTick;
+    }
+
+    Thread.Sleep(REFRESH_INTERVAL_SECONDS * 1000);
 }
 
-var currency = CurrenciesById[CURRENCY_KEY];
-
-CurrencyLogger.Log(currency, "Logs.json");
-
-Predictor.PredictTheNextPrice(new float[]
+static async Task InitAsync()
 {
+    Dictionary<string, Currency> CurrenciesById = new();
+    ApiResponse apiWrapper = await RequestHandler<ApiResponse>.GetDataAsync();
+
+    foreach (var i in apiWrapper.Data)
+    {
+        CurrenciesById.Add(i.Id, i);
+    }
+
+    Currency currency = CurrenciesById[CURRENCY_KEY];
+
+    CurrencyLogger.Log(currency, "Logs.json");
+
+    Predictor.PredictTheNextPrice(new float[]
+    {
     Predictor.CalcutePriceBasedOnChange(currency.CurrentPrice.GetValueOrDefault(), currency.LastMonthPrecentChange.GetValueOrDefault()),
     Predictor.CalcutePriceBasedOnChange(currency.CurrentPrice.GetValueOrDefault(), currency.LastWeekPrecentChange.GetValueOrDefault()),
     Predictor.CalcutePriceBasedOnChange(currency.CurrentPrice.GetValueOrDefault(), currency.LastDayPrecentChange.GetValueOrDefault()),
     Predictor.CalcutePriceBasedOnChange(currency.CurrentPrice.GetValueOrDefault(), currency.LastHourPrecentChange.GetValueOrDefault()),
     currency.CurrentPrice.GetValueOrDefault(),
-});
-
+    });
+}
