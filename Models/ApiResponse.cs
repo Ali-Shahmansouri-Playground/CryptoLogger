c22449a1794dac1ee78c1317a1ff49644d521586
Ali-Shahmansouri-Playground <sctshahali@gmail.com>
2023-10-12 16:31:51 +0330

    Implement models

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLogger.Models
+{
+    public sealed class ApiResponse
+    {
+        [JsonProperty("result")]
+        public List<Currency> Data { get; set; }
+    }
+}
---
+++ Models/Currency.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLogger.Models
+{
+    public sealed class Currency
+    {
+        [JsonProperty("key")]
+        public string? Id { get; set; }
+
+        [JsonProperty("name_en")]
+        public string? Name { get; set; }
+
+        [JsonProperty("price")]
+        public float? CurrentPrice { get; set; }
+
+        [JsonProperty("percent_change_1h")]
+        public float? LastHourPrecentChange { get; set; }
+
+        [JsonProperty("percent_change_24h")]
+        public float? LastDayPrecentChange { get; set; }
+        [JsonProperty("percent_change_7d")]
+        public float? LastWeekPrecentChange { get; set; }
+        [JsonProperty("percent_change_30d")]
+        public float? LastMonthPrecentChange { get; set; }
+    }
+}
