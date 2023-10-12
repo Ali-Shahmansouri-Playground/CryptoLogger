3605d90a79518216e131ed3743b6274ad72fa171
Ali-Shahmansouri-Playground <sctshahali@gmail.com>
2023-10-12 18:33:37 +0330

    Implement the logger logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoLogger.Models;
 using CryptoLogger.Services;
+using CryptoLogger.Utils;
 
 
 // TODO: Implement error handling
@@ -14,4 +15,8 @@ foreach (var i in apiWrapper.Data)
     CurrenciesById.Add(i.Id, i);
     Console.WriteLine(i.Name);
 }
+
+CurrencyLogger.Log(CurrenciesById["BTC"], "Logs.json");
+CurrencyLogger.Log(CurrenciesById["ETH"], "Logs.json");
+CurrencyLogger.Log(CurrenciesById["BTC"], "Logs.json");
 
---
+++ Utils/CurrencyLogger.cs
@@ -0,0 +1,39 @@
+﻿using CryptoLogger.Models;
+using Newtonsoft.Json;
+
+namespace CryptoLogger.Utils
+{
+    public static class CurrencyLogger
+    {
+        private static readonly JsonSerializerSettings options = new()
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+
+        public static void Log(Currency currency, string fileName)
+        {
+            var jsonObject = new List<Currency>();
+            if (File.Exists(fileName))
+            {
+                var data = File.ReadAllText(fileName);
+
+                jsonObject = JsonConvert.DeserializeObject<List<Currency>>(data);
+                jsonObject.Add(currency);
+
+                SerilizeAndSave(jsonObject, fileName);
+            }
+            else
+            {
+                jsonObject.Add(currency);
+                SerilizeAndSave(jsonObject, fileName);
+            }
+
+        }
+
+        private static void SerilizeAndSave(List<Currency> list, string fileName)
+        {
+            var jsonString = JsonConvert.SerializeObject(list, Formatting.Indented, options);
+            File.WriteAllText(fileName, jsonString);
+        }
+    }
+}
